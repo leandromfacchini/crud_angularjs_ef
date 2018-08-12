423b9f53d4e084a1b7e67fa36abf350eb3982dcf
Leandro Facchini <leandromfacchini@gmail.com>
2018-08-12 14:13:26 -0300

    Utilizando rotas

---
+++ BasicoAngularJs/Scripts/main.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="../lib/angular.js" />
+var app = angular.module('blog', ['ngRoute']);
+
+//definindo a rota
+app.config(function ($routeProvider) {
+
+console.log('o puto passou aqui');
+
+    $routeProvider
+        .when('/', { templateUrl: 'views/home.html' })
+        .when('/artigos', { templateUrl: 'views/artigos.html', controller: 'ArtigosController' })
+        .when('/sobre', { templateUrl: 'views/sobre.html' })
+});
+
+app.controller('ArtigosController', function ($scope) {
+
+    $scope.artigos = [
+        "Artigo 1",
+        "Artigo 2",
+        "Artigo 3",
+        "Artigo 4"
+    ];
+});
